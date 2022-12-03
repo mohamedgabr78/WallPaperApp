7b716968d6bd6266026e8e6a2c08e727e2b95dfb
mohamedgabr78 <mo.gabr1994@gmail.com>
2022-12-03 17:32:23 -0500

    First Commit

---
+++ app/src/main/java/algonquin/cst2335/wallpaperapp/MainActivity.java
@@ -0,0 +1,16 @@
+package algonquin.cst2335.wallpaperapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        563492ad6f917000010000014802c08f009e4ab49dd5cc61110d27f3
+    }
+}
